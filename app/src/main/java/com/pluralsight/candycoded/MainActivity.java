a25bf39e4a8952317023e09624ba9509f84ed3d5
Henock <henocknegash86@gmail.com>
2019-10-25 08:17:52 +0300

    First Commit
    Starting The Store information Acitvity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,19 +82,19 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-
-                Intent infoIntent=new Intent(MainActivity.this,InfoActivity.class);
-                startActivity(infoIntent);
-
-        return super.onOptionsItemSelected(item);
-    }
 
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        Intent infoIntent=new Intent(MainActivity.this,InfoActivity.class);
+        startActivity(infoIntent);
+
+        return super.onOptionsItemSelected(item);
+    }
 
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
